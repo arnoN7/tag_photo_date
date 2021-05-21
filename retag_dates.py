e6842b5258c688adb991972cd5c667c81ec5f051
arnoN7 <arnaud.rover@gmail.com>
2021-05-21 23:53:52 +0200

    add delay feature

--- retag_dates.py
+++ retag_dates.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 import glob, os, sys
-from datetime import date
-from datetime import datetime
+from datetime import datetime, timedelta
 import os.path
 import piexif
 import argparse
 from tqdm import tqdm
 import re
+from geotag_photos import get_file_date
 from utils import dir_path
+from geotag_photos import FORMAT
 
 MODIFIED_FILES = 0
 NO_MATCH_FILES = 0
@@ -55,16 +56,41 @@ def retag_date(file):
 
     return True
 
+
+def delay_date(file, offset):
+    filename = os.path.basename(file)
+    exif_dict = piexif.load(file)
+    init_date = datetime.strptime(get_file_date(file, False), FORMAT)
+    print('init-date' + init_date.strftime(FORMAT))
+    date_target = init_date - timedelta(hours=-1)
+    print('Init date ' + get_file_date(file, False) + 'New Date ' + date_target.strftime(FORMAT))
+    if date_target is not None:
+        date_b = date_target.strftime("%Y:%m:%d %H:%M:%S")
+        exif_dict['0th'][piexif.ImageIFD.DateTime] = date_b
+        exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = date_b
+        exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = date_b
+        exif_bytes = piexif.dump(exif_dict)
+        piexif.insert(exif_bytes, file)
+        global MODIFIED_FILES
+        MODIFIED_FILES = MODIFIED_FILES + 1
+
+
+
 def main():
     #try:
         parser = argparse.ArgumentParser()
         parser.add_argument('--path', type=dir_path, required=True, help='path to the folder with photos to tag')
+        parser.add_argument("-o", "--offset", dest="offset", type=int,
+                    help="Hours to delay")
         parsed_args = parser.parse_args()
         os.chdir(parsed_args.path)
         i=0
         for file in tqdm(glob.glob("*.JP*G")):
             i=i+1
-            retag_date(file)
+            if parsed_args.offset is not None:
+                delay_date(file, parsed_args.offset)
+            else:
+                retag_date(file)
         print(str(MODIFIED_FILES) + ' files enriched with dates ')
         print(str(NO_MATCH_FILES) + ' files not updated due to file format error ')
         print(str(INCONSISTENT_EXIF) + ' files updated because of date format error in original EXIF ')
